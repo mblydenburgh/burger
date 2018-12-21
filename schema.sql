846f99e5ab4814061ced0d254177d373e259dbed
M Blydenburgh <mike.blydenburgh@gmail.com>
2018-12-21 14:00:02 -0500

    initial files

---
+++ schema.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS burger_db;
+
+CREATE DATABASE burger_db;
+
+USE burger_db;
+
+CREATE TABLE burgers (
+	id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    burger VARCHAR(255),
+    eaten BOOLEAN
+);
+
+INSERT INTO burgers (burger,eaten)
+VALUES ("BBQ Burger",false), ("Bacon Cheese Burger",false), ("Southwestern Burger",false);
